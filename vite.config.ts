5365ac961007e02a421fda1b7713ecf0a822a85c
leonardo <leonardocamelo20@gmail.com>
2025-02-07 18:36:58 -0300

    ♻ refactor: add aliases for common files

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
@@ -7,5 +8,12 @@ export default defineConfig({
     setupFiles: './vitest-setup.js',
     environment: 'jsdom',
     globals: true,
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
   }
 });
